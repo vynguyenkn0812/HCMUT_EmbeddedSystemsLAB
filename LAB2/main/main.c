4316ab15a34418ac13febeab0f773a3cc746bd12
Nguyen Phuc Tien <nguyenphuctien1258@gmail.com>
2023-09-22 19:55:15 +0700

    updated button debounce

--- LAB2/main/main.c
+++ LAB2/main/main.c
@@ -1,8 +1,6 @@
 /*
- * SPDX-FileCopyrightText: 2010-2022 Espressif Systems (Shanghai) CO LTD
- *
- * SPDX-License-Identifier: CC0-1.0
- */
+ * Created by Nhom 6
+*/
 
 #include <stdio.h>
 #include <inttypes.h>
@@ -12,10 +10,8 @@
 #include "freertos/task.h"
 #include "esp_chip_info.h"
 #include "esp_flash.h"
-
-#define CONFIG_LED_PIN 2
-#define ESP_INTR_FLAG_DEFAULT 0
-#define CONFIG_BUTTON_PIN 0
+#include "FreeRTOSConfig.h"
+#include "button.h"
 
 TaskHandle_t ISR = NULL;
 
@@ -23,14 +19,17 @@ static void vPrintId()
 {
     while (1)
     {
-        printf("%s", "2014725\r\n");
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        // Get tick count at wakeup time
+        TickType_t xLastWakeupTime = xTaskGetTickCount();
+
+        printf("%s", "Student ID: 2014725\r\n");
+        xTaskDelayUntil(xLastWakeupTime, 1000 / portTICK_PERIOD_MS);
     }
 }
 
 void IRAM_ATTR button_isr_handler(void *arg)
 {
-    xTaskResumeFromISR(ISR);
+    if (is_button_pressed()) xTaskResumeFromISR(ISR);
 }
 
 void vButtonIsr(void *arg)
@@ -46,19 +45,20 @@ void app_main(void)
 {
     esp_rom_gpio_pad_select_gpio(CONFIG_BUTTON_PIN);
 
-    // set the correct direction
+    // Set the correct direction
     gpio_set_direction(CONFIG_BUTTON_PIN, GPIO_MODE_INPUT);
 
-    // enable interrupt on falling (1->0) edge for button pin
+    // Enable interrupt on falling (1->0) edge for button pin
     gpio_set_intr_type(CONFIG_BUTTON_PIN, GPIO_INTR_NEGEDGE);
 
     // Install the driver’s GPIO ISR handler service, which allows per-pin GPIO interrupt handlers.
     // install ISR service with default configuration
     gpio_install_isr_service(ESP_INTR_FLAG_DEFAULT);
 
-    // attach the interrupt service routine
+    // Attach the interrupt service routine
     gpio_isr_handler_add(CONFIG_BUTTON_PIN, button_isr_handler, NULL);
 
+    // Create 2 FreeRTOS tasks
     xTaskCreate(vPrintId, "print id", 4096, NULL, 1, NULL);
     xTaskCreate(vButtonIsr, "button isr", 4096, NULL, 1, &ISR);
 }
