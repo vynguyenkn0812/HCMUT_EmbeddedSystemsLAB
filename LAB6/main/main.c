7b3304a296845c806968d94c833d69a5198d3b7a
Nguyen Phuc Tien <nguyenphuctien1258@gmail.com>
2023-10-04 17:41:42 +0700

    create LAB6 project

---
+++ LAB6/main/main.c
@@ -0,0 +1,18 @@
+/*
+ * Created by Nhom 6
+*/
+
+#include <stdio.h>
+#include <inttypes.h>
+#include "driver/gpio.h"
+#include "sdkconfig.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+#include "esp_chip_info.h"
+#include "esp_flash.h"
+#include "FreeRTOSConfig.h"
+
+void app_main(void) {
+
+}
---
+++ LAB6/main/usr_wifi.h
@@ -0,0 +1,15 @@
+#ifndef USR_WIFI_H
+#define USR_WIFI_H
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include <esp_event.h>
+#include <esp_event_loop.h>
+#include <esp_wifi.h>
+#include <esp_err.h>
+
+BaseType_t WIFI_ON();
+BaseType_t WIFI_DISCONNECT();
+BaseType_t 
+
+#endif
