e99a2388ebb9678382ba353eeb4bb8695c1b1595
dangnguyenblackie <howard.nguyen@nbowree.com>
2023-10-29 12:09:02 +0700

    Add busyWait to task1

--- LAB3/main.c
+++ LAB3/main.c
@@ -14,16 +14,25 @@
 #include "esp_system.h"
 
 
-char msg[] = "This is our text to testing LAB3";
+char msg[] = "This is our text to test LAB3";
 
 TaskHandle_t task1 = NULL;
 TaskHandle_t task2 = NULL;
+
+void busyWait(int millisec){
+    int64_t start = esp_timer_get_time(); // in microsec
+    int64_t end = start + millisec * 1000
+    while(esp_timer_get_time() < end){
+        // nothing
+    }
+}
 
 void task1(){
     int len = strlen(msg);
     while(true){
         for(int i=0; i < len; i+=1){
             printf("%c",msg[i]);
+            busyWait(50);
         }
         printf("\n");
         vTaskDelay(1000/portTICK_PERIOD_MS);
